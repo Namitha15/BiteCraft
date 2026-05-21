db1661f073f774957004b5c6528758293d1625a8
Namitha Udupa <namithaudupa1510@gmail.com>
2026-05-21 15:19:26 +0530

    3rd commit without map

--- BiteCraft/src/components/Body.jsx
+++ BiteCraft/src/components/Body.jsx
@@ -1,14 +1,58 @@
-import React from "react";
-import resObj from "../utils/mockData.js";
+import resObj from "../utils/mockData";
 import RestaturantCard from "./ResturantCard";
 
 const Body = () => {
   return (
-    <div className="res-container">
-      {resObj.restaurants.map((resturant) => (
-        <RestaturantCard key={resturant.id} resData={resturant.info} />
-      ))}
+    <div className="body">
+      <div className="res-container">
+        {/* {resObj.restaurants.map((resturant) => (
+          <RestaturantCard key={resturant.info.id} resData={resturant.info} />
+        ))} */}
+        <RestaturantCard resData={resObj.restaurants[0].info} />
+        <RestaturantCard resData={resObj.restaurants[1].info} />
+        <RestaturantCard resData={resObj.restaurants[2].info} />
+        <RestaturantCard resData={resObj.restaurants[3].info} />
+        <RestaturantCard resData={resObj.restaurants[4].info} />
+      </div>
     </div>
   );
 };
+
 export default Body;
+
+// import React, { useState } from "react";
+// import resObj from "../utils/mockData.js";
+// import RestaturantCard from "./ResturantCard";
+
+// const Body = () => {
+//   // Master copy of all restaurants (never mutated directly)
+//   const [listOfRestaurants, setListOfRestaurants] = useState(resObj.restaurants);
+
+//   // The actual list rendered on screen
+//   const [filteredResturant, setFilteredResturant] = useState(resObj.restaurants);
+
+//   return (
+//     <div className="body">
+//       <div className="filter-search">
+//         <div
+//           className="filter-btn"
+//           onClick={() => {
+//             // Filter using the master copy, but update the filtered state.
+//             // Using > 4.5 because all restaurants in mock data are > 4.0.
+//             const filtered = listOfRestaurants.filter((res) => res.info.avgRating > 4.5);
+//             setFilteredResturant(filtered);
+//           }}
+//         >
+//           Top Rated Restaurant
+//         </div>
+//       </div>
+//       <div className="res-container">
+//         {filteredResturant.map((resturant) => (
+//           <RestaturantCard key={resturant.info.id} resData={resturant.info} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Body;
--- BiteCraft/src/components/ResturantCard.jsx
+++ BiteCraft/src/components/ResturantCard.jsx
@@ -2,23 +2,21 @@ import { CDN_URL } from "../utils/constants";
 
 const RestaturantCard = (props) => {
   const { resData } = props;
-  console.log(resData);
+  // console.log(resData);
   const { name, cloudinaryImageId, cuisines, avgRating, costForTwo } = resData;
 
   return (
-    <div className="res-container">
-      <div className="res-card">
-        <img
-          className="res-logo"
-          src={CDN_URL + cloudinaryImageId}
-          alt="image loading"
-        ></img>
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src={CDN_URL + cloudinaryImageId}
+        alt="image loading"
+      ></img>
 
-        <h3>{name}</h3>
-        <h3>Rating{avgRating}⭐</h3>
-        <h3 className="cuisines">{cuisines?.join(",")}</h3>
-        <h3 className="cuisines">{costForTwo}</h3>
-      </div>
+      <h3>{name}</h3>
+      <h3>Rating{avgRating}⭐</h3>
+      <h3 className="cuisines">{cuisines?.join(",")}</h3>
+      <h3 className="cuisines">{costForTwo}</h3>
     </div>
   );
 };
